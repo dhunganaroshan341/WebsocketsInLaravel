3b63d9c9b429f727fd512ec86d2d0009a69d5b2b
Roshan Dhungana <dhunganaroshan341@gmail.com>
2026-06-30 23:22:15 +0545

    feat websocket initilization

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-//
+import "./echo";
+
+console.log("🚀 App started");
+
+window.Echo.channel("hello-world")
+    .listen("HelloWorldEvent", (e) => {
+        console.log("🎉 Event received!", e);
+    });
